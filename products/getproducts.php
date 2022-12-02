d6ad1d3f3a4003049dcc06b0adc4d55c46e6155b
Jennynny <101732141+Jennynny@users.noreply.github.com>
2022-12-02 13:23:14 +0200

    hakee tuotteet

---
+++ products/getproducts.php
@@ -0,0 +1,29 @@
+<?php
+require_once "../inc/functions.php";
+require_once "../inc/headers.php";
+
+//"bäkkärissä luetaan osoitteesta viimeiset osa eli / ja numero."
+$uri = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH); //luetaan osoite. Frontista tuleva kutsu.
+$parameters = explode('/',$uri); //"hajottaa osoiteen palasiksi"
+$tuoteryhmanro = $parameters[1]; //lukee kauttaviivalla erotetun numeron eli viimeisen numeron. Herjaa edelleen tätä rivi.
+
+
+
+try {
+    $db =openDb();
+    $sql = "select * from tuoteryhma where tuoteryhmanro = $tuoteryhmanro";
+    $query = $db->query($sql);
+    $category = $query->fetch(PDO::FETCH_ASSOC);
+
+    $sql = "select * from tuote where tuoteryhmanro = $tuoteryhmanro";
+    $query = $db->query($sql);
+    $products = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    header('HTTP/1.1 200 OK');
+    echo json_encode(array(
+       "category" => $category['tuoteryhmanimi'],
+       "products" => $products 
+    ),JSON_PRETTY_PRINT);
+}catch (PDOException $pdoex){
+    returnError($pdoex);
+}
