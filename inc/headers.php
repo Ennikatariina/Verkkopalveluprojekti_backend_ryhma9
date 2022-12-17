3f36cdb2844cd25e97413bf2e80c6d98205e5ce8
Jennynny <101732141+Jennynny@users.noreply.github.com>
2022-12-17 22:33:07 +0200

    lisäys addproductiin

--- inc/headers.php
+++ inc/headers.php
@@ -7,7 +7,7 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
     // you want to allow, and if so:
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day   
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day 
 }
 
 // Access-Control headers are received during OPTIONS requests
--- products/addproduct.php
+++ products/addproduct.php
@@ -14,14 +14,16 @@ $price = filter_var($input->hinta,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $category_id = filter_var($input->tuoteryhmanro,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 //pitääkö tehdä kuvakselle tällainen rivi kans?
+$description = filter_var($input->kuvaus,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 try {
     $db = openDb();
     //$sql = "insert into product (name,price,image,category_id) values ('$name',$price,'placeholder.png',$category_id)";
-    $sql = "insert into tuote (tuotenimi,kuvaus,kuvannimi,hinta,tuoteryhmanro) values ('$name','varakuva.jpg',$price,$category_id)";
+    $sql = "insert into tuote (tuotenimi,kuvaus,kuvannimi,hinta,tuoteryhmanro) values ('$name','$description','varakuva.jpg',$price,$category_id)";
+    //järjestys tuotetaulun mukaan.
     executeInsert($db,$sql);
-    //$data = array('id' => $db->lastInsertId(),'name' => $name,'price' => $price, 'image' => 'placeholder.png'); //puuttuu kuvaus.
-    $data = array('tuotenro' => $db->lastInsertId(),'tuotenimi' => $name,'hinta' => $price, 'kuvannimi' => 'varakuva.jpg');
+    //$data = array('id' => $db->lastInsertId(),'name' => $name,'price' => $price, 'image' => 'placeholder.png'); //Lisätty kuvaus.
+    $data = array('tuotenro' => $db->lastInsertId(),'tuotenimi' => $name,'kuvaus'=> $description,'hinta' => $price, 'kuvannimi' => 'varakuva.jpg');
     print json_encode($data);
 }
 catch (PDOException $pdoex) {
