82f07db0a920a0e136f91626864e81c206e80082
jonnanne <101732110+jonnanne@users.noreply.github.com>
2022-12-18 18:51:13 +0200

    changed headers

--- inc/headers.php
+++ inc/headers.php
@@ -14,8 +14,8 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        // may also be using PUT, PATCH, HEAD etc
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        // may also be using PATCH, HEAD etc
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
