aac412fcf306f0bcec1c827a0e325596435b8efb
Your Name <you@example.com>
2022-12-19 21:37:20 +0200

    admin osion luonti

--- user_login/rest_login.php
+++ user_login/rest_login.php
@@ -8,9 +8,15 @@ require("./db_user_controller.php");
 //kun käyttäjä yrittää kirjautua, niin katsotaan onko sessio jo käynnissä eli käyttäjä kirjautuneena
 if(isset($_SESSION['kayttajatunnus'])){
     http_response_code(200);
-    echo $_SESSION['kayttajatunnus'];
+    //echo $_SESSION['kayttajatunnus'];
+    //palauttaa tiedon käyttäjästä ja admin oikeuksista? pitää adminin kirjautuneena sivua päivittäessä
+    $data["username"]=$_SESSION['kayttajatunnus'];
+    $data["admin"]=$_SESSION['admin'];
+    echo json_encode($data);
     return;
 }
+
+
 
 //Onko asetettu post-parametria: onko määritelty kayttajatunnus parametria ja salasana parametria
 if(!isset($_POST['kayttajatunnus']) || !isset($_POST['salasana'])){
@@ -23,10 +29,14 @@ $uname= $_POST['kayttajatunnus'];
 $pw= $_POST['salasana'];
 
 $verified_uname= checkUser($uname, $pw);
+$admin= checkAdmin($uname);
 if($verified_uname){
     $_SESSION["kayttajatunnus"]=$verified_uname;
+    $_SESSION["admin"]=$admin;
     http_response_code(200);
-    echo $verified_uname;
+    $data["username"]=$verified_uname;
+    $data["admin"]=$admin;
+    echo json_encode($data);
 }else{
     http_response_code(401);
     echo"Wrong username or password.";
