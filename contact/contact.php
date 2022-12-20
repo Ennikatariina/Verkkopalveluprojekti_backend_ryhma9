20beccb911c77b0069b6daf1c6a8b7eede2ec0ce
jonnanne <101732110+jonnanne@users.noreply.github.com>
2022-12-20 16:26:58 +0200

    sends feedback to mysql

--- contact/contact.php
+++ contact/contact.php
@@ -5,14 +5,24 @@ require("../inc/functions.php");
 
 $body = file_get_contents("php://input");
 $dataObject =json_decode($body);
+$db = openDb();
 
 // tallentaa yhteyslomake tauluun viestit
 function insertcontact($db, $etunimi, $sukunimi, $email, $puhnro, $viesti){
-   $sql= "INSERT INTO yhteyslomake (etunimi, sukunimi, email, puhnro, viesti) VALUES (?, ?, ?, ?, ?, ?)";
+   $sql= "INSERT INTO yhteyslomake (etunimi, sukunimi, email, puhnro, viesti) VALUES (?, ?, ?, ?, ?)";
     $statement = $db->prepare($sql);
     $statement->execute(array($etunimi, $sukunimi, $email, $puhnro, $viesti));
     $viimeinenViesti=  $db->lastInsertId();
     return $viimeinenViesti;
     }
+
+    $etunimi = $dataObject->etunimi;
+    $sukunimi = $dataObject->sukunimi;
+    $email = $dataObject->email;
+    $puhnro = $dataObject->puhnro;
+    $viesti = $dataObject->viesti;
+
+
+    insertcontact($db, $etunimi, $sukunimi, $email, $puhnro, $viesti);
 
     
